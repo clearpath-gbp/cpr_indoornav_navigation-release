8484436a8f02add0147611812b13d03555c13689
Chris Iverach-Brereton <civerachb@clearpathrobotics.com>
2024-08-26 17:17:17 -0400

    Trimmed the branch to only the cpr_indoornav_navigation_msgs sub directory

--- cpr_indoornav_navigation/setup.py
+++
@@ -1,9 +0,0 @@
-from distutils.core import setup
-from catkin_pkg.python_setup import generate_distutils_setup
-
-d = generate_distutils_setup(
-    packages=['cpr_indoornav_navigation'],
-    package_dir={'': 'src'}
-)
-
-setup(**d)
